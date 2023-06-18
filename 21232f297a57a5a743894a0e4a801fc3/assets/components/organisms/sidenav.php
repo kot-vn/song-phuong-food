9d78f163b2c1f8ad15d48fe321799d182325be3d
kotvn-kienvt <kvstudio.154@gmail.com>
2023-06-18 15:21:08 +0700

    remove target blank

--- 21232f297a57a5a743894a0e4a801fc3/assets/components/organisms/sidenav.php
+++ 21232f297a57a5a743894a0e4a801fc3/assets/components/organisms/sidenav.php
@@ -1,7 +1,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main" data-color="success">
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0" href="" target="_blank">
+    <a class="navbar-brand m-0" href="javascript:void(0)">
       <img src="<?= getPageFloor(0) ?>assets/img/favicon/android-chrome-256x256.png" class="navbar-brand-img h-100" alt="main_logo">
       <span class="ms-1 font-weight-bold">Song Phuong Food</span>
     </a>
